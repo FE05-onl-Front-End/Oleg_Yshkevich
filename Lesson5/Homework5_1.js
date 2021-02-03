5397396d5e0a13cba104808678b33577dc35b60e
olegyshkevich <olegyshkevich23@gmail.com>
2021-02-03 19:31:20 +0300

    Oleg_Yshkevich-hm_5--Objects

---
+++ Lesson5/Homework5_1.js
@@ -0,0 +1,12 @@
+// Превое задание
+const data = 3;
+const valueToFill = 'a';
+
+const fill = (arraySize, value) => {
+  array = [];
+  for(let i = 0; i < arraySize; i++){
+    array.push(value);
+  }
+  return array;
+}
+console.log(fill(data, valueToFill))
